f7d4c7901b05d8246f3cf4581117239c54c5c321
Dale Weber <hybotix@gmail.com>
2026-04-27 05:08:17 +0000

    bridge-test — test heap allocation of SparkFun_VL53L5CX after Bridge.provide()

--- Arduino/UNO-Q/bridge-test/sketch/sketch.ino
+++ Arduino/UNO-Q/bridge-test/sketch/sketch.ino
@@ -1,17 +1,7 @@
 #include <Arduino_RouterBridge.h>
-
-// Disable all VL53L5CX result fields except distance_mm and target_status
-#define VL53L5CX_DISABLE_AMBIENT_PER_SPAD
-#define VL53L5CX_DISABLE_NB_TARGET_DETECTED
-#define VL53L5CX_DISABLE_NB_SPADS_ENABLED
-#define VL53L5CX_DISABLE_SIGNAL_PER_SPAD
-#define VL53L5CX_DISABLE_RANGE_SIGMA_MM
-#define VL53L5CX_DISABLE_REFLECTANCE_PERCENT
-#define VL53L5CX_DISABLE_MOTION_INDICATOR
-
 #include <SparkFun_VL53L5CX_Library.h>
 
-SparkFun_VL53L5CX myImager;
+SparkFun_VL53L5CX *myImager = nullptr;
 VL53L5CX_ResultsData measurementData;
 
 String echo(String value) {
@@ -21,6 +11,7 @@ String echo(String value) {
 void setup() {
     Bridge.begin();
     Bridge.provide("echo", echo);
+    myImager = new SparkFun_VL53L5CX();
 }
 
 void loop() {
